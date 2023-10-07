8976d7f49546bc3521c521d11f640e4286779802
ayazkeyani <81766023+ayazkeyani@users.noreply.github.com>
2023-10-07 20:26:58 +0100

    Update Program.cs
    First Application

--- CollectionOfApps/Program.cs
+++ CollectionOfApps/Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int intRows = 0;
+int intColumns = 0;
+
+Console.WriteLine("Number of columns?");
+string stringColumns = Console.ReadLine();
+intColumns = CheckInput(stringColumns, intColumns, "columns");
+
+Console.WriteLine("Number of rows?");
+string stringRows = Console.ReadLine();
+intRows = CheckInput(stringRows, intRows, "rows");
+
+GenerateText(intRows, intColumns);
+static int CheckInput(string value, int intValue, string context)
+{
+    while (!Int32.TryParse(value, out intValue))
+    {
+        Console.WriteLine("Not valid input");
+
+        value = Console.ReadLine();
+    }
+
+    intValue = Convert.ToInt32(value);
+    return intValue;
+}
+
+static void GenerateText(int rows, int columns)
+{
+    // nested loop
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write("@");
+        }
+        Console.WriteLine("");
+    }
+}
+
+Console.ReadKey();
